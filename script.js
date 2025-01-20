9c3850c146769cd29c5222ffbb29773f6d864a74
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-20 18:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+
+
+let btn = document.getElementById('btn');
+let output = document.getElementById('output');
+
+btn.addEventListener('click', submitInput);
+async function submitInput(){
+let text = document.getElementById('text').value.trim();
+let delay = document.getElementById('delay').value;
+let delayNumber = Number(delay);
+	if(!delay || delay<=0 ){
+		output.innerHTML = `<p>Please Enter Dealy in milliseconds</p>`
+		return
+	}
+  await	new Promise((resolve , reject)=>{
+	  		setTimeout(()=>{
+		   output.innerHTML = `<p>${text}</p>`
+				resolve();
+	  },delayNumber);
+  document.getElementById('text').value = '';
+  document.getElementById('delay').value = '';
+  })
+}
+
+
+
+
